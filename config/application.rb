9971c3c46a9b54567de5e709f0a8618b75e5fa00
electricdrop <electricdrop@users.noreply.github.com>
2016-09-16 18:02:10 +0300

    Add devise views

--- config/application.rb
+++ config/application.rb
@@ -13,12 +13,15 @@ module Boxes
     # -- all .rb files in that directory are automatically loaded.
 
     config.generators do |g|
-      g.test_framework      :rspec, fixture: true
-      g.fixture_replacement :factory_girl
-      g.view_specs          false
-      g.helper              false
-      g.helper_specs        false
-      g.assets              false
+      g.test_framework      :rspec, fixture: true # Use RSpec as test framework
+      g.fixture_replacement :factory_girl # Use factories instead of fixtures
+      g.view_specs          false # Do not generate view specs
+      g.helper              false # Do not generate helpers
+      g.helper_specs        false # Do not generate helper specs
+      g.assets              false # Do not generate assets
     end
+
+    # Do not wrap invalid form fields because it breaks layout
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root 'static_pages#home'
-  get 'overview', to: 'static_pages#overview'
+
   get 'about', to: 'static_pages#about'
+
+  authenticate :user do
+    get 'overview', to: 'static_pages#overview'
+  end
+
 end
