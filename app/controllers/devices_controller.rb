843cc00d53250774b8c8ac5e3ba3ef93a96f43e2
electricdrop <electricdrop@users.noreply.github.com>
2016-09-27 18:40:30 +0300

    Add device views

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,37 +1,52 @@
 class DevicesController < ApplicationController
 
   def index
-
+    @devices = Device.all
   end
 
   def show
-
+    @device = Device.find(params[:id])
   end
 
   def new
-
+    @device = Device.new
   end
 
   def edit
-
+    @device = Device.find(params[:id])
   end
 
   def create
-
+    @device = Device.new(device_params)
+    if @device.save
+      flash[:success] = "Device added"
+      redirect_to @device
+    else
+      render 'new'
+    end
   end
 
   def update
-
+    @device = Device.find(params[:id])
+    if @device.update_attributes(device_params)
+      flash[:success] = "#{@device.name} saved"
+      redirect_to @device
+    else
+      render 'edit'
+    end
   end
 
   def destroy
     Device.find(params[:id]).destroy
+    flash[:warning] = "Device deleted"
+    redirect_back(fallback_location: devices_url)
   end
 
   private
 
   def device_params
-
+    params.require(:device).permit(:name, :devtype, :model, :state, :ip,
+                                   :location, :sn, :site_id)
   end
 
 end
