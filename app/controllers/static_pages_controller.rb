ef2512c536d1eae6f2d06a064a21f5cc0979b9a0
electricdrop <electricdrop@users.noreply.github.com>
2016-10-04 18:09:34 +0300

    Add primary site settings

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,12 +6,20 @@ class StaticPagesController < ApplicationController
   end
 
   def overview
-    @supplies_ending = Supply.includes(:site).ending_soon.order("quantity")
-    @supplies_most_used = Supply.includes(:site).most_used
-    @devices_repair = Device.includes(:site).in_repair.order("name")
     @sites = Site.all
     @devices = Device.all
     @supplies = Supply.all
+    @primary_site =  current_user.settings.primary_site
+    if @primary_site.nil?
+      @supplies_ending = Supply.includes(:site).ending_soon.order("quantity")
+      @supplies_most_used = Supply.includes(:site).most_used
+      @devices_repair = Device.includes(:site).in_repair.order("name")
+    else
+      @primary_site_name = Site.find(@primary_site).name
+      @supplies_ending = Supply.includes(:site).where(site_id: @primary_site).ending_soon.order("quantity")
+      @supplies_most_used = Supply.includes(:site).where(site_id: @primary_site).most_used
+      @devices_repair = Device.includes(:site).where(site_id: @primary_site).in_repair.order("name")
+    end
   end
 
   def about
