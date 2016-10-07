22aa41885b59dd86297c19458884ccd68f339aee
electricdrop <electricdrop@users.noreply.github.com>
2016-10-07 18:43:55 +0300

    Switch to MariaDB in all the environments

--- app/models/version.rb
+++ app/models/version.rb
@@ -4,10 +4,6 @@ class Version < ApplicationRecord
   validates :used, presence: true, numericality: { greater_than_or_equal_to: 0,
                                                    less_than: 2147483648 }
 
-  # This is MySQL version, will use it in production
-  # scope :by_year, lambda { |year| where('extract(year from created_at) = ?', year) }
-
-  scope :by_year, lambda { |year| where("strftime('%Y', created_at) = ?",
-                                        "#{year}") } # SQLite version
+  scope :by_year, lambda { |year| where('extract(year from created_at) = ?', year) }
 
 end
