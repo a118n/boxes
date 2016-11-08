925207f70660696c0975010a8f8f83f7ae919b52
a118n <4118nd@gmail.com>
2016-11-08 16:52:58 +0300

    Guess seeds are important for tests...

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 3.times { FactoryGirl.create(:site_with_everything) }
+3.times { FactoryGirl.create(:site_with_everything) }
