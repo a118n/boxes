28c885be28836e6af08efc078f53c4fc174de00e
a118n <4118nd@gmail.com>
2016-11-21 17:53:25 +0300

    ActionMailer multiple recipients fix

--- app/mailers/supply_mailer.rb
+++ app/mailers/supply_mailer.rb
@@ -2,9 +2,7 @@ class SupplyMailer < ApplicationMailer
   def ending_notification(supply)
     @supply = supply
     @users = User.notifiable
-    @users.each do |user|
-      mail(to: user.email, subject: "Supply #{@supply.name} is ending")
-    end
+    mail(to: @users.map(&:email).uniq, subject: "Supply #{@supply.name} is ending")
   end
 
   def monthly_usage(month, year)
@@ -13,8 +11,6 @@ class SupplyMailer < ApplicationMailer
     # Get all the supplies using scope defined in Model
     @supplies = Supply.all_used
     @users = User.notifiable
-    @users.each do |user|
-      mail(to: user.email, subject: "Supplies usage for #{@month} #{@year}")
-    end
+    mail(to: @users.map(&:email).uniq, subject: "Supplies usage for #{@month} #{@year}")
   end
 end
