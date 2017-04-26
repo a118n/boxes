6dacab45a54a8c3b88738022d75975ffd7a2c0e9
Daniel Allen <dallen@cnrua000777.de.obi.net>
2017-04-26 12:35:32 +0300

    Authorization

--- app/mailers/supply_mailer.rb
+++ app/mailers/supply_mailer.rb
@@ -1,7 +1,7 @@
 class SupplyMailer < ApplicationMailer
   def ending_notification(supply)
     @supply = supply
-    @users = User.where(site: @supply.site_id).notifiable
+    @users = User.where(site: @supply.site_id).or(User.where(admin: true)).notifiable
     mail(to: @users.map(&:email).uniq, subject: "Supply #{@supply.name} is ending")
   end
 
@@ -9,7 +9,7 @@ class SupplyMailer < ApplicationMailer
     @month = month
     @year = year
     @site = site
-    @users = User.where(site: @site.id).notifiable
+    @users = User.where(site: @supply.site_id).or(User.where(admin: true)).notifiable
     @supplies = Supply.where(site_id: @site.id).all_used
     mail(to: @users.map(&:email).uniq, subject: "Supplies usage in #{@site.name} for #{@month} #{@year}")
   end
