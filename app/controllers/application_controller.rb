0aeab4457de9a7d31588f45b75eb0468d0115416
a118n <a118n@users.noreply.github.com>
2017-04-28 13:16:50 +0300

    Redirect to root if not authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
 
   before_action :redirect_to_search, except: :search
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.html { redirect_to root_path, alert: exception.message }
+    end
+  end
+
   private
 
   def redirect_to_search
