fbc1af3d510d013b097f19b4edb6c77f3441ae56
a118n <a118n@users.noreply.github.com>
2017-06-14 17:39:57 +0300

    Fix mysql issue when creating tables

--- db/migrate/20170614074728_rolify_create_roles.rb
+++ db/migrate/20170614074728_rolify_create_roles.rb
@@ -1,13 +1,13 @@
 class RolifyCreateRoles < ActiveRecord::Migration[5.0]
   def change
-    create_table :roles, options: 'ROW_FORMAT=DYNAMIC' do |t|
+    create_table :roles, options: 'DEFAULT CHARSET=utf8' do |t|
       t.string :name
       t.references :resource, polymorphic: true
 
       t.timestamps
     end
 
-    create_table :users_roles, id: false, options: 'ROW_FORMAT=DYNAMIC' do |t|
+    create_table :users_roles, id: false, options: 'DEFAULT CHARSET=utf8' do |t|
       t.references :user
       t.references :role
     end
