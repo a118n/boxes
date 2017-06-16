32a97d8fe160deab25739938f42dffc22f3225c0
a118n <a118n@users.noreply.github.com>
2017-06-16 10:50:04 +0300

    Fix for devices and supplies creation not authorized by users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,8 +7,10 @@ class Ability
       can :manage, :all
     elsif user.has_role? :user
       can :read, Site, id: Site.with_role(:user, user).pluck(:id)
-      can [:read, :update, :assign, :all, :create, :export, :destroy], Device, site_id: Site.with_role(:user, user).pluck(:id)
-      can [:read, :update, :assign, :history, :all, :create, :export, :destroy], Supply, site_id: Site.with_role(:user, user).pluck(:id)
+      can [:read, :update, :assign, :all, :export, :destroy], Device, site_id: Site.with_role(:user, user).pluck(:id)
+      can :create, Device
+      can [:read, :update, :assign, :history, :all, :export, :destroy], Supply, site_id: Site.with_role(:user, user).pluck(:id)
+      can :create, Supply
       can :manage, :static_page
     else
       can [:home, :about], :static_page
