51b8db9fc8ea62bb41ef7aec9ac8dfd4a5c1780f
a118n <a118n@users.noreply.github.com>
2017-06-23 14:19:16 +0300

    Rewrite history to use paper_trail

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -105,16 +105,19 @@ class SuppliesController < ApplicationController
   end
 
   def history
-    # @site = Site.find(params[:site_id])
-    # @supply = @site.supplies.find(params[:id])
-    # @start_year = (@supply.versions.first.try(:created_at) || Date.today).year
-    # if params[:report_date]
-    #   @year = params[:report_date][:year]
-    #   @versions # Stub!
-    #   # @pie_chart_data =  @versions.map { |v| [v.created_at.strftime("%B"), v.used] }.to_h
-    # end
-    flash[:alert] = "This feature is currently unavailable"
-    redirect_to site_supply_path
+    @site = Site.find(params[:site_id])
+    @supply = @site.supplies.find(params[:id])
+    @start_year = (@supply.versions.first.try(:created_at) || Date.today).year
+    if params[:report_date]
+      @year = params[:report_date][:year].to_i
+      @versions = []
+      (1..12).each do |month|
+        @date = Date.new(@year, month, -1)
+        @version = @supply.versions.where(created_at: @date.beginning_of_day .. @date.end_of_day).last
+        @versions << @version unless @version.nil?
+      end
+      @pie_chart_data =  @versions.map { |v| [v.created_at.strftime("%B"), v.reify.used] }.to_h
+    end
   end
 
   private
